17351f3d83f44f15f6af57a5b2d66b3081339991
Nathan Fishel <50148601+nfishel48@users.noreply.github.com>
2019-12-04 14:08:47 -0500

    empty classes to inint repository

---
+++ External.java
@@ -0,0 +1,10 @@
+/*
+@author Nathaniel Fishel
+@assignment Project 2
+@semester Fall 2019
+@class COSC 311
+@professor Dr. Haynes
+@description This class will demonstrate that a external merge sort is O(n log n)
+ */
+public class External {
+}
