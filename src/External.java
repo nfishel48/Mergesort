1e5f2189339917a1ea0e9653dd11fd29e91d4ad0
Nathan Fishel <50148601+nfishel48@users.noreply.github.com>
2019-12-04 14:37:00 -0500

    l

---
+++ src/External.java
@@ -0,0 +1,31 @@
+/*
+@author Nathaniel Fishel
+@assignment Project 2
+@semester Fall 2019
+@class COSC 311
+@professor Dr. Haynes
+@description This class will demonstrate that a external merge sort is O(n log n)
+ */
+import java.util.*;
+public class External {
+    static Random r = new Random();
+    static Scanner stdIn = new Scanner(System.in);
+
+
+    static int[] sort(int[] arr){
+        return arr;
+    }
+
+
+    public static int[] genInts(){
+        System.out.println("How large should the array be? ");
+        int input = stdIn.nextInt();
+        int[] arr = new int[input];
+        return arr;
+   }
+
+
+    public static void main(){
+
+    }
+}
